a72421f17972b636acf9c69d8277ba1643477c84
Andrew Kubera <andrewkubera@gmail.com>
2015-05-17 01:27:58 -0400

    Added zero, equality, addition and subtraction functions to ThreeVector, with tests.

--- src/vectors/threevector.rs
+++ src/vectors/threevector.rs
@@ -4,6 +4,7 @@
 ///
 
 use super::PhysicalVector;
+use std;
 
 pub
 struct ThreeVector {
@@ -18,6 +19,42 @@ impl PhysicalVector for ThreeVector {
   fn mag2(&self) -> f64
   {
     self.x * self.x + self.y * self.y + self.z * self.z
+  }
+
+}
+
+impl super::super::num::Zero for ThreeVector {
+  fn zero() -> ThreeVector {ThreeVector {x:0.0, y:0.0, z:0.0}}
+}
+
+impl std::ops::Add for ThreeVector {
+  type Output = ThreeVector;
+
+  fn add(self, rhs:ThreeVector) -> Self::Output
+  {
+    ThreeVector {x: self.x + rhs.x,
+                 y: self.y + rhs.y,
+                 z: self.z + rhs.z}
+  }
+}
+
+impl std::ops::Sub for ThreeVector {
+  type Output = ThreeVector;
+
+  fn
+  sub(self, rhs:ThreeVector) -> Self::Output
+  {
+    ThreeVector {x: self.x - rhs.x,
+                 y: self.y - rhs.y,
+                 z: self.z - rhs.z}
+  }
+}
+
+impl std::cmp::PartialEq for ThreeVector {
+
+  fn eq(&self, other:&ThreeVector) -> bool
+  {
+    self.x == other.x && self.y == other.y && self.z == other.z
   }
 
 }
--- tests/test_vectors.rs
+++ tests/test_vectors.rs
@@ -16,6 +16,27 @@ fn test_threevector()
   assert_eq!(v.mag(), (14.0 as f64).sqrt());
 }
 
+#[test]
+fn test_threevector_equality()
+{
+  let a = ThreeVector {x: 1.0, y: 2.0, z: 3.0};
+  let b = ThreeVector {x: 1.0, y: 2.0, z: 3.0};
+  let r = a == b;
+  assert!(r);
+}
+
+#[test]
+fn test_threevector_addition()
+{
+  let u = ThreeVector {x: 1.0, y: 2.0, z: 3.0};
+  let v = ThreeVector {x: 1.0, y: 2.0, z: 3.0};
+  let s = ThreeVector {x: 2.0, y: 4.0, z: 6.0};
+  let r = u + v;
+  let q = r == s;
+  assert!(q);
+}
+
+
 
 #[test]
 fn test_lorentz_vector_constructor()
